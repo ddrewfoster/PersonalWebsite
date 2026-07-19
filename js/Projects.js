d2f6ca5634c3b7444ff5ceba874ff3f7b99ba16a
Drew Foster <destinibarcelona@yahoo.com>
2026-07-19 13:51:15 -0500

    migrating to different computer

---
+++ js/Projects.js
@@ -0,0 +1,13 @@
+class Project {
+    constructor(types, links, experiences, images) {
+        this.type = type;
+        this.link = link;
+        this.experience = experience;
+        this.image = image;
+    }
+
+    filterProjectSkills() {
+        return ":";
+    }
+
+}
