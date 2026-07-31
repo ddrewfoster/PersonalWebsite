8692cb6c74ac35d67ada52516b8f95a92f5ef33c
Drew Foster <destinibarcelona@yahoo.com>
2026-07-31 10:57:06 -0500

    reverted data js to a const rather than interpreting as API

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,20 @@
+const things = document.getElementById('contact');  
+var location = 0;
+document.getElementById('contact').addEventListener('click', function() {
+    const dataContainer = document.getElementById('work');
+    document.getElementsByClassName("off");
+    dataContainer.innerHTML = '<p>working</p>';
+})
+
+
+
     async function fetchProjects() {
         try {
             const response = await fetch('data.js');
             const data = await response.json();
 
             console.log(data);
+            things.innerText = `${data}`;
         }
         catch(error) {
             console.error("Failed to retrieve JSON: ", error);
